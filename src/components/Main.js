b4e2fde1d2e1b87abeaf8f1cec1146be841b1e29
Choiji92 <jeahoon100@naver.com>
2022-06-13 17:26:27 +0900

    백엔드협업중

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,74 @@
-import styled from 'styled-components'
-import React from "react";
+import styled from "styled-components";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import { addHeart, addHeartDB, minusHeartDB } from '../redux/modules/contentSlice';
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import {
+  addHeart,
+  addHeartDB,
+  loadContentDB,
+  minusHeartDB,
+} from "../redux/modules/contentSlice";
 
 const Main = () => {
   const data = useSelector((state) => state.content.content_list);
-  console.log(data);
-  const dispatch = useDispatch()
+  console.log(data.length);
+  const dispatch = useDispatch();
+  const [target, setTarget] = useState(null);
+  const [page, setPage] = useState(2)
+  // 무한스크롤 관련 intersection observer
+  const onIntersect = async ([entry], observer) => {
+    //entry.isIntersecting은 내가 지금 target을 보고있니?라는 뜻 그 요소가 화면에 들어오면 true 그전엔 false
+    if (entry.isIntersecting) {
+      observer.unobserve(entry.target); // 이제 그 target을 보지 않겠다는 뜻
+      await dispatch(loadContentDB(page));
+      console.log(page)
+    }
+  };
+  useEffect(() => {
+    let observer;
+    if (target) {
+      setPage(page+1)
+      observer = new IntersectionObserver(onIntersect, {
+        threshold: 1,
+      });
+      observer.observe(target); // target을 보겠다!
+      
+    }
+    return () => {
+      observer && observer.disconnect();
+    };
+  }, [target]);
   return (
     <Container>
-      {data.map((v) => (
-        <Card key={v.id}>
-            <Head>
+      {data.map((v,i) => (
+        <Card key={v.id} ref={i === data.length - 1 ? setTarget : null}>
+          <Head>
             <div>닉네임</div>
-            <div>{v.date}</div>
-            </Head>
+            <div>{v.createdAt}</div>
+          </Head>
           <img src={v.imageUrl}></img>
-          {!v.heart_count.includes('jeahoon10000@naver.com') ?
-          <FavoriteBorderIcon onClick={() => {
-              dispatch(addHeartDB(v.id,'jeahoon10000@naver.com'))
-           
-          }} fontSize='large' cursor='pointer'></FavoriteBorderIcon>
-          :
-          <FavoriteIcon style={{color:'red'}} onClick={() => {
-              dispatch(minusHeartDB(v.id,'jeahoon10000@naver.com'))
-           
-          }} fontSize='large' cursor='pointer'></FavoriteIcon>
-        }
+          {/* {!v.heart_count.includes("jeahoon10000@naver.com") ? (
+            <FavoriteBorderIcon
+              onClick={() => {
+                dispatch(addHeartDB(v.id, "jeahoon10000@naver.com"));
+              }}
+              fontSize="large"
+              cursor="pointer"
+            ></FavoriteBorderIcon>
+          ) : (
+            <FavoriteIcon
+              style={{ color: "red" }}
+              onClick={() => {
+                dispatch(minusHeartDB(v.id, "jeahoon10000@naver.com"));
+              }}
+              fontSize="large"
+              cursor="pointer"
+            ></FavoriteIcon>
+          )} */}
+          {/* <div>좋아요 {v.heart_count.length}개</div> */}
           <h1>{v.title}</h1>
-          <div>{v.content}</div>
+          <div >{v.content.length < 30 ? v.content : v.content.slice(0,30)+'...'}</div>
         </Card>
       ))}
     </Container>
@@ -39,27 +77,32 @@ const Main = () => {
 
 const Container = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   flex-wrap: wrap;
   width: 95%;
   margin: 0 auto;
 `;
 const Card = styled.div`
   width: 25%;
-    margin: 20px 20px;
-    border: 1px solid;
-    border-radius: 5px;
-    padding: 10px;
-  img{
-      width: 100%;
-      height: 400px;
+  margin: 20px 20px;
+  border: 1px solid;
+  border-radius: 10px;
+  padding: 10px;
+  img {
+    width: 100%;
+    height: 400px;
+  }
+  :hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 5px 15px 0px;
   }
 `;
 const Head = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: center;
-    margin-bottom: 10px;
-`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 10px;
+`;
 export default Main;
--- src/redux/modules/contentSlice.js
+++ src/redux/modules/contentSlice.js
@@ -1,98 +1,130 @@
 import { async } from "@firebase/util";
 import { createSlice } from "@reduxjs/toolkit";
-import axios from 'axios'
+import axios from "axios";
+import instance from "./axios";
 //middlewares
 
-export const loadContentDB = () => {
-    return async function(dispatch){
-        await axios.get('http://localhost:5001/content').then(response => {
-        dispatch(loadContent(response.data))
-      })
-    }
-}
-export const createContentDB = (data) => {
-    return async function(dispatch){
-        await axios.post('http://localhost:5001/content',data).then(response => {
-        console.log(response)    
-        dispatch(createContent(response.data))
-        })
-    }
-}
-export const updateContentDB = (data) => {
-    return async function(dispatch){
-        console.log('수정하러왔어')
-        console.log(data)
-        await axios.put(`http://localhost:5001/content/${data.id}`,data).then(response => {
-            dispatch(updateContent(response.data))
-            // console.log(response)
-        })
-    }
-}
-export const deleteContentDB = () => {
-    return async function(dispatch){
-        
-    }
-}
-export const addHeartDB = (content_id,user_id) => {
-    return async function(dispatch,getState){
-        console.log(content_id)
-        console.log(user_id)
-        const content = getState().content.content_list.filter(v => v.id === Number(content_id))
-        const new_heart = content[0].heart_count
-        const heart = [...new_heart,user_id]
-        await axios.patch(`http://localhost:5001/content/${content_id}`,{
-            heart_count: heart
-        }).then(reponse => {
-            dispatch(addHeart(reponse.data))
-        })
-        
-        }
-    }
-    export const minusHeartDB = (content_id,user_id) => {
-        return async function(dispatch,getState){
-            console.log(content_id)
-            console.log(user_id)
-            const content = getState().content.content_list.filter(v => v.id === Number(content_id))
-            const new_heart = content[0].heart_count.filter(v => v !== user_id)
-            console.log(new_heart)
-            await axios.patch(`http://localhost:5001/content/${content_id}`,{
-                heart_count: new_heart
-            }).then(reponse => {
-                console.log('나는 리스폰스',reponse)
-                dispatch(minusHeart(reponse.data))
-            })
-            
-            }
-        }
+export const loadContentDB = (page) => {
+  return async function (dispatch, getState) {
+    await instance
+      .get("/api/post/list", { params: { _page: page, _limit: 4 } })
+      .then((response) => {
+        console.log(response.data.contents);
+        const data = getState().content.content_list;
+        console.log('나는데이터',data)
+        const new_data = [...data, ...response.data.contents];
+        console.log(new_data);
+        dispatch(loadContent(new_data));
+      });
+  };
+};
 
+export const heartLoadContentDB = () => {
+  return async function (dispatch) {
+    await instance.get("/content");
+  };
+};
+export const createContentDB = (data) => {
+  return async function (dispatch) {
+    await instance.post("/api/post", data).then((response) => {
+      console.log(response);
+      dispatch(createContent(response.data));
+    });
+  };
+};
+export const updateContentDB = (data) => {
+  return async function (dispatch) {
+    console.log("수정하러왔어");
+    console.log(data);
+    await instance.put(`/api/post/${data.id}/modify`, data).then((response) => {
+      dispatch(updateContent(response.data));
+      // console.log(response)
+    });
+  };
+};
+export const deleteContentDB = () => {
+  return async function (dispatch) {};
+};
+export const addHeartDB = (content_id, user_id) => {
+  return async function (dispatch, getState) {
+    console.log(content_id);
+    console.log(user_id);
+    const content = getState().content.content_list.filter(
+      (v) => v.id === Number(content_id)
+    );
+    const new_heart = content[0].heart_count;
+    const heart = [...new_heart, user_id];
+    await instance
+      .patch(`/content/${content_id}`, {
+        heart_count: heart,
+      })
+      .then((reponse) => {
+        dispatch(addHeart(reponse.data));
+      });
+  };
+};
+export const minusHeartDB = (content_id, user_id) => {
+  return async function (dispatch, getState) {
+    console.log(content_id);
+    console.log(user_id);
+    const content = getState().content.content_list.filter(
+      (v) => v.id === Number(content_id)
+    );
+    const new_heart = content[0].heart_count.filter((v) => v !== user_id);
+    console.log(new_heart);
+    await instance
+      .patch(`/content/${content_id}`, {
+        heart_count: new_heart,
+      })
+      .then((reponse) => {
+        console.log("나는 리스폰스", reponse);
+        dispatch(minusHeart(reponse.data));
+      });
+  };
+};
 
 const contentSlice = createSlice({
-    name:'content',
-    initialState : {
-        content_list:[]
+  name: "content",
+  initialState: {
+    content_list: [],
+  },
+  reducers: {
+    loadContent: (state, action) => {
+      state.content_list = action.payload;
     },
-    reducers: {
-        loadContent: (state, action) => {
-            state.content_list= action.payload
-        },
-        createContent:(state,action) =>{
-            state.content_list.push(action.payload)
-        },
-        updateContent:(state,action) =>{
-            const index = state.content_list.findIndex(v => v.id === action.payload.id)
-            state.content_list[index]=action.payload
-        },
-        addHeart:(state,action) => {
-            const index = state.content_list.findIndex(v => v.id === action.payload.id)
-            state.content_list[index]=action.payload
-        },
-        minusHeart:(state,action) => {
-            const index = state.content_list.findIndex(v => v.id === action.payload.id)
-            state.content_list[index]=action.payload
-        }
+    heartLoadContent: (state, action) => {
+      state.content_list = action.payload;
+    },
+    createContent: (state, action) => {
+      state.content_list.push(action.payload);
+    },
+    updateContent: (state, action) => {
+      const index = state.content_list.findIndex(
+        (v) => v.id === action.payload.id
+      );
+      state.content_list[index] = action.payload;
+    },
+    addHeart: (state, action) => {
+      const index = state.content_list.findIndex(
+        (v) => v.id === action.payload.id
+      );
+      state.content_list[index] = action.payload;
+    },
+    minusHeart: (state, action) => {
+      const index = state.content_list.findIndex(
+        (v) => v.id === action.payload.id
+      );
+      state.content_list[index] = action.payload;
+    },
+  },
+});
 
-    }
-})
-
-export const {loadContent,createContent,updateContent,addHeart,minusHeart} = contentSlice.actions
-export default contentSlice.reducer
+export const {
+  loadContent,
+  heartLoadContent,
+  createContent,
+  updateContent,
+  addHeart,
+  minusHeart,
+} = contentSlice.actions;
+export default contentSlice.reducer;
